598af63e4978d271257f8647287dbaea0eb46d26
SimLeek <josh.miklos@gmail.com>
2017-11-02 19:11:27 -0700

    updated ui for file tree. Displays current directory now.

--- src/main/java/Browsing/FileTree.java
+++ src/main/java/Browsing/FileTree.java
@@ -2,19 +2,43 @@ package Browsing;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeModel;
+import javax.swing.tree.DefaultTreeModel;
 import java.io.File;
 
 public class FileTree extends JTree {
     //https://stackoverflow.com/a/23808238/782170
 
-    public FileTree(TreeModel mod, File fileRoot, DefaultMutableTreeNode root){
-        super(mod);
+    private DefaultMutableTreeNode root;
+
+    public FileTree(File fileRoot){
+        //super();
 
         setShowsRootHandles(true);
 
+        this.root = new DefaultMutableTreeNode(".");
+
+
+        this.treeModel = new DefaultTreeModel(this.root);
+
         CreateChildren ccd =
-                new CreateChildren(fileRoot, root);
+                new CreateChildren(fileRoot, this.root);
+        new Thread(ccd).start();
+
+    }
+
+    public FileTree(){
+        super();
+
+        setShowsRootHandles(true);
+
+        File fileRoot = new File(".");
+
+        this.root = new DefaultMutableTreeNode(".");
+
+        this.treeModel = new DefaultTreeModel(this.root);
+
+        CreateChildren ccd =
+                new CreateChildren(fileRoot, this.root);
         new Thread(ccd).start();
     }
 }
--- src/main/java/TextFileAnalyzer.java
+++ src/main/java/TextFileAnalyzer.java
@@ -1,8 +1,10 @@
-import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
-import java.awt.event.*;
+import Browsing.FileTree;
 
-public class TextFileAnalyzer extends JDialog {
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.File;
+
+public class TextFileAnalyzer extends JDialog{
     private JPanel contentPane;
     private JButton newButton;
     private JButton browseButton;
@@ -10,27 +12,13 @@ public class TextFileAnalyzer extends JDialog {
     private JButton analyzeButton;
     private JButton helpButton;
     private JButton averageButton;
-    private JTree tree1;
+    private FileTree tree1;
     private JEditorPane editorPane1;
 
 
-    public TextFileAnalyzer() {
+    TextFileAnalyzer() {
         setContentPane(contentPane);
         setModal(true);
-        //getRootPane().setDefaultButton(buttonOK);
-
-        /*buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
-
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
-        */
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -46,8 +34,6 @@ public class TextFileAnalyzer extends JDialog {
                 onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-
-        DefaultMutableTreeNode top = new DefaultMutableTreeNode(".");
 
     }
 
@@ -67,4 +53,12 @@ public class TextFileAnalyzer extends JDialog {
         dialog.setVisible(true);
         System.exit(0);
     }
+
+    private void createUIComponents() {
+        File fileRoot = new File(".");
+        this.tree1 = new FileTree(fileRoot);
+        this.tree1.updateUI();
+
+    }
+
 }
